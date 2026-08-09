7132aefede3e786205f812d8b94255d73b18022e
Melvin Snijders <25155102+MelvinSnijders@users.noreply.github.com>
2026-08-09 20:28:26 +0200

    feat: modify footer to link to GitHub profile

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -15,18 +15,9 @@ export default async function SiteFooter() {
     <footer className="border-t-8 border-black bg-white px-6 py-6 text-sm">
       <div className="mx-auto flex w-full max-w-7xl flex-col items-center justify-between gap-4 text-center font-semibold text-black/70 uppercase sm:flex-row sm:text-left">
         <p>
-          © {new Date().getFullYear()} Lootje.app — gemaakt met 💛 door Melvin
-          Snijders
+          © {new Date().getFullYear()} Lootje.app — gemaakt met 💛 door <a href="https://github.com/MelvinSnijders">Melvin
+          Snijders</a>
         </p>
-        <Button asChild variant={"yellow"} size={"sm"}>
-          <a
-            href="https://tikkie.me/pay/drht82mirkof1fvjd122"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Coffee /> Trakteer op koffie
-          </a>
-        </Button>
       </div>
     </footer>
   );
